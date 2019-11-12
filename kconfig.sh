6be3b76fc450fcb9789f9991ad4ab07f5aa1ed9e
Tom <twitchyliquid64@ciphersink.net>
2019-11-11 22:21:29 -0800

    Enabled md support, additional device drivers

---
+++ kconfig.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# ./kconfig.sh <path-to-almost-built-system>
+
+SCRIPT_BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+#Determine linux version from the sources.
+LIN=`ls $1/linux-*`
+LIN="${LIN##*/}"
+K_VERS=`echo $LIN | sed -e "s/^linux-//" | cut -d" " -f1| sed -e "s/://"`
+
+# Prepare source tree
+chroot $1 bash -c "cd \"/linux-${K_VERS}\" && make mrproper"
+
+cp -v "${SCRIPT_BASE_DIR}/resources/linux/.config" "${1}/linux-${K_VERS}/.config"
+
+chroot $1 bash -c "cd \"/linux-${K_VERS}\" && make menuconfig"
